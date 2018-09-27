5baa2e0f48a67a230e4ec79cbd37146d0addd20b
Vanessa Ariel <vanessa.ariel24@gmail.com>
2018-09-27 09:53:52 -0300

    Fix display flex

--- script/script.js
+++ script/script.js
@@ -1,44 +1,75 @@
 const button = document.querySelector(".btn-tarefas");
 const input = document.querySelector(".form__input");
 const list = document.querySelector(".list-tarefas");
+const checkAll = document.querySelector(".btn-check-all");
+const deleteAll = document.querySelector(".btn-delete-all");
+const boxAll =  document.querySelector(".box-all");
 
-button.addEventListener("click", function (e){
+
+button.addEventListener("click", function(e) {
     e.preventDefault();
     //alert("Oi!");
-    
-    if(input.value === "" || input.value === " "){
+
+
+    if (input.value.length === 0 || !input.value.trim()) {
         return false;
     }
     //ADD LINHA
     const linha = document.createElement("li");
     linha.className = "list-item";
 
-    //INSERE TEXTO DO INPUT NA CONST
+    //INSERE TEXTO DO INPUT NA LINHA
     linha.innerHTML = (input.value);
 
     //ADD LINHA DENTRO DA LISTA
     list.appendChild(linha);
-    
+
     //LIMPA INPUT AO ENVIAR INPUT
     input.value = "";
 
     //ADD CHECK NOS ITENS
-    linha.addEventListener("click", function(e){
+    linha.addEventListener("click", function(e) {
+        e.preventDefault();
         linha.style.textDecoration = "line-through"
-        linha.style.color = "gray"; 
-    })
+        linha.style.color = "gray"
 
+    })
     const span = document.createElement("span");
+    //ADD CLASSE NO SPAN
     span.className = "list-tarefas__span"
-    span.innerHTML= `<button class="list-tarefas__botao-excluir">x</button>`
-    console.log(span.innerHTML)
-    span.addEventListener("click", function(event){
-        event.preventDefault();
-        linha.remove()
-    })
+        //ADD CLASSE BOTÃO NO SPAN DIRETO NO HTML
+    span.innerHTML = `<button class="list-tarefas__botao-excluir">x</button>`
 
-    linha.appendChild(span)   
-    
+    span.addEventListener("click", function(event) {
+            event.preventDefault();
+            //DELETA LINHA QUANDO CLICA NO X
+            linha.remove()
+        })
+        // ADD SPAN NA LINHA   
+    linha.appendChild(span)
+
+    //PARA VISUALIZAR OS BOTÕES DENTRO DO OUTRO BOTÃO
+    // deleteAll.style.display = "flex";
+    // checkAll.style.display = "flex";
+
+    boxAll.style.display = "flex";
+    boxAll.style.justifyContent = "space-evenly";
 })
 
-
+checkAll.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    //ADD LINHA
+    const item = document.querySelectorAll(".list-item");
+    //FOR PARA RISCAR E MUDAR COR
+    for (let checado of item) { // mesma coisa que for normal [array]
+        checado.style.textDecoration = "line-through"
+        checado.style.color = "gray"
+    }
+})
+// EVENTO CLICK PARA REMOVER HTML E LIMPAR ESTILOS
+deleteAll.addEventListener("click", function(evn) {
+    evn.preventDefault();
+    list.innerHTML = ""
+    //REMOVER O ESTILO TODA VEZ QUE DELETA
+    list.removeAttribute("style");
+})
